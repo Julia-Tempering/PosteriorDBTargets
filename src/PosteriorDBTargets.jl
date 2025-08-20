f199984e0c3e754e9399fd3f4a1b220caa12f4f3
Alexandre Bouchard <bouchard@stat.ubc.ca>
2025-08-20 14:01:36 -0700

    Updated convention for providing targets and ids

--- src/PosteriorDBTargets.jl
+++ src/PosteriorDBTargets.jl
@@ -8,11 +8,11 @@ using BridgeStan
 #       and we would still need to bridge to Pigeons so maybe not high priority.
 include("$(dirname(dirname(Base.pathof(Pigeons))))/test/supporting/postdb.jl")
 
-targetIds() = map(posterior_db_list(false)) do string 
+provide_targetIds() = map(posterior_db_list(false)) do string 
     without_suffix = string[begin:end-5]
     return Symbol(without_suffix)
 end
 
-build_target(symbol) = log_potential_from_posterior_db("$symbol.json")
+provide_target(symbol) = log_potential_from_posterior_db("$symbol.json")
 
 end 
