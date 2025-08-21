d72a77769391accbfd7c8d255cb26106b3a3ffc7
Alexandre Bouchard <bouchard@stat.ubc.ca>
2025-08-20 19:39:45 -0700

    Add setup

--- src/PosteriorDBTargets.jl
+++ src/PosteriorDBTargets.jl
@@ -4,9 +4,14 @@ using Pigeons
 using BridgeStan
 
 # TODO: move that here, or switch over to PosteriorDB.jl 
-#       (but we do not need the extra features of that package at the moment), 
-#       and we would still need to bridge to Pigeons so maybe not high priority.
+#       (but we do not need the extra features of that package at the moment, 
+#       and we would still need to bridge to Pigeons so maybe not high priority).
 include("$(dirname(dirname(Base.pathof(Pigeons))))/test/supporting/postdb.jl")
+
+installed() = isdir(Pigeons.mpi_settings_folder() * "/posteriordb/posterior_database")
+if !installed() 
+    setup_posterior_db() 
+end
 
 provide_targetIds() = map(posterior_db_list(false)) do string 
     without_suffix = string[begin:end-5]
